bb17c25d82d7187d1094edb41853e82568a7d32f
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-04-11 11:23:19 +0100

    set erlang path for mac/linux

--- erlang.el
+++ erlang.el
@@ -1,12 +1,19 @@
+
+
+(defvar erlang_home
+  (if (eq system-type 'darwin)
+      "/opt/local/lib/erlang/"
+      "/usr/lib64/erlang/"))
+
 (require 'auto-complete-config)
+
 (add-to-list 'ac-dictionary-directories "~/.emacs/.cask/24.4.1/elpa/auto-complete-20150201.150/dict/")
 (ac-config-default)
 
-
-(setq load-path (cons "/usr/lib64/erlang/lib/tools-2.7.1/emacs"
+(setq load-path (cons (concat erlang_home "lib/tools-2.7.1/emacs")
 		      load-path))
 (setq erlang-root-dir "/usr/lib64/erlang/")
-(setq exec-path (cons "/usr/lib64/erlang/bin" exec-path))
+(setq exec-path (cons (concat erlang_home "bin") exec-path))
 (require 'erlang-start)
 
 (add-hook 'erlang-mode-hook '(lambda() (setq indent-tabs-mode nil)))
