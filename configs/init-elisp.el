ed63f674f153c0928d682a9bde13bdd3e3084a7a
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-04-17 23:53:20 +0100

    improved elisp config

--- configs/init-elisp.el
+++ configs/init-elisp.el
@@ -2,6 +2,24 @@
 (defun add-mode-to-list (mode)
   (add-to-list 'auto-mode-alist '("\\.el\\'" . (lambda ()
 						 (emacs-lisp-mode)
-						 (rainbow-delimiters-mode)
-						 (show-paren-mode)
 						 ))))
+(add-hook 'emacs-lisp-mode-hook
+	  (lambda ()
+	    ;; Use spaces, not tabs.
+	    (setq indent-tabs-mode nil)
+	    ;; Keep M-TAB for `completion-at-point'
+	    (define-key flyspell-mode-map "\M-\t" nil)
+	    ;; Pretty-print eval'd expressions.
+	    (define-key emacs-lisp-mode-map
+	      "\C-x\C-e" 'pp-eval-last-sexp)
+	    ;; Recompile if .elc exists.
+	    (add-hook (make-local-variable 'after-save-hook)
+		      (lambda ()
+			(byte-force-recompile default-directory)))
+	    (define-key emacs-lisp-mode-map
+	      "\r" 'reindent-then-newline-and-indent)))
+
+(add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode) ;; Requires Ispell
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
