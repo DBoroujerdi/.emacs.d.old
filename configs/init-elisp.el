8bab705809bacaabe15d7ccfc13769949bc4cd63
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-04-17 23:40:49 +0100

    fixed elisp config

--- configs/init-elisp.el
+++ configs/init-elisp.el
@@ -1,13 +1,7 @@
 
 (defun add-mode-to-list (mode)
-  (add-to-list 'auto-mode-alist '("\\.el\\". mode)))
-
-(defun elisp-modes (mode-list)
-  (dolist (m mode-list)
-    (add-mode-to-list m)))
-
-(elisp-modes
- '(
-   rainbow-delimiters-mode
-   show-paren-mode
-   ))
+  (add-to-list 'auto-mode-alist '("\\.el\\'" . (lambda ()
+						 (emacs-lisp-mode)
+						 (rainbow-delimiters-mode)
+						 (show-paren-mode)
+						 ))))
