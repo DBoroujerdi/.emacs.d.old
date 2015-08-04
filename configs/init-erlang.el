c244fd51bb430cbd52d0765b01704ec4c38e3faf
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2015-08-04 12:17:58 +0100

    fixed erlang edit to work on linux box

--- configs/init-erlang.el
+++ configs/init-erlang.el
@@ -3,14 +3,18 @@
 (defvar erlang_home
   (if (eq system-type 'darwin)
       "/opt/local/lib/erlang/"
-      "/usr/lib64/erlang/"))
+    "/usr/lib64/erlang/"))
+
+(defvar tools_dir
+  (if (eq system-type 'darwin)
+      "lib/tools-2.7.2/emacs" "lib/tools-2.7.1/emacs"))
 
 (require 'auto-complete-config)
 
 (add-to-list 'ac-dictionary-directories "~/.emacs/.cask/24.4.1/elpa/auto-complete-20150201.150/dict/")
 (ac-config-default)
 
-(setq load-path (cons (concat erlang_home "lib/tools-2.7.2/emacs")
+(setq load-path (cons (concat erlang_home tools_dir)
 		      load-path))
 (setq erlang-root-dir erlang_home)
 (setq exec-path (cons (concat erlang_home "bin") exec-path))
