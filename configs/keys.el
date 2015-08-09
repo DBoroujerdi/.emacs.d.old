af14f5c4d161f4696e3eed826cecde17845c3981
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-08-09 19:59:41 +0100

    extra key bindings

--- configs/keys.el
+++ configs/keys.el
@@ -3,6 +3,9 @@
 
 (global-set-key (kbd "C-x c") 'comment-region)
 (global-set-key (kbd "C-x x") 'uncomment-region)
+(global-set-key (kbd "C-w") 'backward-kill-word)
+(global-set-key (kbd "C-x C-k") 'kill-region)
+(global-set-key (kbd "C-c C-k") 'kill-region)
 
 ;; window tabbing
 (global-set-key [C-tab] 'other-window)
