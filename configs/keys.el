e82349bf76983d2d40ea912379e894a9a9c4cfcf
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2015-09-03 17:58:58 +0100

    removed stupid key mapping

--- configs/keys.el
+++ configs/keys.el
@@ -3,7 +3,6 @@
 
 (global-set-key (kbd "C-x c") 'comment-region)
 (global-set-key (kbd "C-x x") 'uncomment-region)
-(global-set-key (kbd "C-w") 'backward-kill-word)
 (global-set-key (kbd "C-x C-k") 'kill-region)
 (global-set-key (kbd "C-c C-k") 'kill-region)
 
