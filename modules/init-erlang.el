e11d4673c23911df027bf03e0fcb7aa40dfbefbf
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-09-06 15:26:55 +0100

    fixed erlang config

--- modules/init-erlang.el
+++ modules/init-erlang.el
@@ -1,14 +1,11 @@
 
 (defvar erlang_home "/home/dborouje/apps/erlang/")
 
-(add-to-list
- 'load-path
- (car
-  (file-expand-wildcards
-   "/home/dborouje/apps/erlang/lib/tools-*/emacs/")))
+(setq load-path (cons "/home/dborouje/apps/erlang/lib/tools-*/emacs/" load-path))
 
 (setq erlang-root-dir erlang_home)
 (setq exec-path (cons (concat erlang_home "bin") exec-path))
+
 (require 'erlang-start)
 
 (add-hook 'erlang-mode-hook '(lambda() (setq indent-tabs-mode nil)))
