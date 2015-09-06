4c62fd4c574104c7a9a4160428ab9b7ba225d766
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-09-06 18:37:31 +0100

    smex mode

--- modules/keys.el
+++ modules/keys.el
@@ -22,4 +22,11 @@
                   (t (self-insert-command (or arg 1)))))
 
 
+;; smex
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+
 (provide 'keys)
--- modules/theme.el
+++ modules/theme.el
@@ -43,4 +43,8 @@
 
 (golden-ratio-mode 1)
 
+(require 'smex) ; Not needed if you use package.el
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
+                                        ; when Smex is auto-initialized on its first run.
+
 (provide 'theme)
