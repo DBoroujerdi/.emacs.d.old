283bf5bb4c27d2fc20eb585606aab6974d25dd01
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2015-10-13 12:29:51 +0100

    ido comments

--- modules/init-ido.el
+++ modules/init-ido.el
@@ -5,20 +5,23 @@
 (require 'ido-ubiquitous)
 (ido-ubiquitous-mode 1)
 
-(require 'smex) ; Not needed if you use package.el
-(smex-initialize) ; Can be omitted. This might cause a (minimal) delay
-                                        ; when Smex is auto-initialized on its first run.
+;; smart command completion
+(require 'smex)
+(smex-initialize)
 
-
+;; vertical ido completion
 (require 'ido-vertical-mode)
 (ido-mode 1)
 (ido-vertical-mode 1)
 (setq ido-vertical-define-keys 'C-n-and-C-p-only)
 
+;; fuzzy ido matching
 (setq ido-enable-flex-matching t)
 
+;; start completion with word at cursor point
 (setq ido-use-filename-at-point 'guess)
 
+;; allow configuring of ignore extensions
 (setq ido-ignore-extensions t)
 
 (provide 'init-ido)
