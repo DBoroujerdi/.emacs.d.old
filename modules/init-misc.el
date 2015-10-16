c0d9c849a09f19d27776cb14e4251bac881df3e8
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2015-10-16 13:52:02 +0100

    comments

--- modules/init-misc.el
+++ modules/init-misc.el
@@ -1,8 +1,11 @@
 
+;; allow ? help command where available
 (global-discover-mode 1)
 
+;; js indent level to 4 spaces
 (setq js-indent-level 4)
 
+;; no tabs allowed
 (setq-default indent-tabs-mode nil)
 
 (provide 'init-misc)
