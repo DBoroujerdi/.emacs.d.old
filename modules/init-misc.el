54af08d24a60a619cb5d840ae41e6b60a4d1dcd9
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-10-30 18:40:48 +0000

    ediff config

--- modules/init-misc.el
+++ modules/init-misc.el
@@ -11,4 +11,11 @@
 ;; enable command log - logs commands!
 (require 'command-log-mode)
 
+;; ediff config..
+;; makes ediff operate in the selected frame
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+;; makes ediff horizontally split
+(setq ediff-split-window-function 'split-window-horizontally)
+;; TODO more ediff config to add.. http://emacswiki.org/emacs/EdiffMode
+
 (provide 'init-misc)
