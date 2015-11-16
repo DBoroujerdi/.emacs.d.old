9425607724ab06eb33a2e3b51409d3ffb8b63586
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2015-11-16 20:03:57 +0000

    Cmd as Meta

--- modules/init-osx.el
+++ modules/init-osx.el
@@ -1,6 +1,11 @@
 ;; Allow hash to be entered
 (global-set-key (kbd "M-3") '(lambda () (interactive) (insert "#")))
 
+;; Cmd as meta
+(setq mac-option-key-is-meta nil)
+(setq mac-command-key-is-meta t)
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier nil)
 
 ;; only for OSX - loads path
 (when (memq window-system '(mac ns))
