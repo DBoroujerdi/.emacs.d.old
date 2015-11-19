e49c5b5a10fc1f8d0eba68932460f5539a3a3ef9
Daniel Boroujerdi <daniel.boroujerdi@williamhill.com>
2015-11-19 11:52:19 +0000

    javascript indent level to 2

--- modules/init-misc.el
+++ modules/init-misc.el
@@ -2,8 +2,8 @@
 ;; allow ? help command where available
 ;; (global-discover-mode 1)
 
-;; js indent level to 4 spaces
-(setq js-indent-level 4)
+;; js indent level to 2 spaces
+(setq js-indent-level 2)
 
 ;; no tabs allowed
 (setq-default indent-tabs-mode nil)
