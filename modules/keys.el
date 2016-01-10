a036fdb21b917905669957da9544753e5dbdf135
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-01-10 11:05:59 +0000

    move windows with shift-arrow

--- modules/keys.el
+++ modules/keys.el
@@ -48,6 +48,11 @@
 (global-set-key (kbd "C-c <up>")    'windmove-up)
 (global-set-key (kbd "C-c <down>")  'windmove-down)
 
+(global-set-key (kbd "S-<left>")  'windmove-left)
+(global-set-key (kbd "S-<right>") 'windmove-right)
+(global-set-key (kbd "S-<up>")    'windmove-up)
+(global-set-key (kbd "S-<down>")  'windmove-down)
+
 ;; make a new frame
 ;; (global-set-key (kbd "M-n") 'make-frame)
 
