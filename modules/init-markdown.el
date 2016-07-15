2fd864f7ea2900d2a0e6e05ce6b9476a6af24dfc
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2016-07-15 21:28:04 +0100

    configure markdown-mode with use-package

--- modules/init-markdown.el
+++ modules/init-markdown.el
@@ -1,10 +1,12 @@
 
 ;; ===== markdown
 
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
 
 (provide 'init-markdown)
