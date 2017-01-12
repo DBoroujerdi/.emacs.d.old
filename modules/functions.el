9ff6bdc68e11f04ef7ffecd47e19ce9522e76ed8
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2017-01-12 17:04:46 +0000

    functions for displaying available monospace fonts
    ripped from the internet
       - https://gist.github.com/haxney/3055728

--- modules/functions.el
+++ modules/functions.el
@@ -70,4 +70,38 @@ directory to make multiple eshell windows easier."
   (my-iswitchb-close))
 
 
+;; Display all the monospace fonts available to Emacs in a dedicated buffer
+(defun font-is-mono-p (font-family)
+  ;; with-selected-window
+  (let ((wind (selected-window))
+        m-width l-width)
+   (with-current-buffer "*Monospace Fonts*"
+     (set-window-buffer (selected-window) (current-buffer))
+     (text-scale-set 4)
+     (insert (propertize "l l l l l" 'face `((:family ,font-family))))
+     (goto-char (line-end-position))
+     (setq l-width (car (posn-x-y (posn-at-point))))
+     (newline)
+     (forward-line)
+     (insert (propertize "m m m m m" 'face `((:family ,font-family) italic)))
+     (goto-char (line-end-position))
+     (setq m-width (car (posn-x-y (posn-at-point))))
+     (eq l-width m-width))))
+
+(defun compare-monospace-fonts ()
+  "Display a list of all monospace font faces."
+  (interactive)
+  (pop-to-buffer "*Monospace Fonts*")
+
+  (erase-buffer)
+  (dolist (font-family (font-family-list))
+    (when (font-is-mono-p font-family)
+      (let ((str font-family))
+        (newline)
+        (insert
+         (propertize (concat "The quick brown fox jumps over the lazy dog 1 l; 0 O o ("
+                             font-family ")\n") 'face `((:family ,font-family)))
+         (propertize (concat "The quick brown fox jumps over the lazy dog 1 l; 0 O o ("
+                             font-family ")\n") 'face `((:family ,font-family) italic)))))))
+
 (provide 'functions)
