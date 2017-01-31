a4abc3d4e2eb67a0c8b70809572018d0a14571e3
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2017-01-31 17:14:46 +0000

    config for react development

--- modules/init-js.el
+++ modules/init-js.el
@@ -1,28 +1,96 @@
 
+;;
+;; normal js development
+;;
+
 ;; https://github.com/skeeto/skewer-mode
 ;; http://emacs.stackexchange.com/questions/2376/how-to-use-skewer-mode
 
 ;; js indent level to 2 spaces
-(setq js-indent-level 4)
+(setq js-indent-level 2)
 
-;; js2 mode
-(autoload 'js2-mode "js2-mode" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; ;; js2 mode
+;; (autoload 'js2-mode "js2-mode" nil t)
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
-;; jsx mode
-(add-to-list 'auto-mode-alist '("\\.jsx$" . jsx-mode))
+;; ;; skewer mode
+;; (add-hook 'js2-mode-hook 'skewer-mode)
+;; (add-hook 'css-mode-hook 'skewer-css-mode)
+;; (add-hook 'html-mode-hook 'skewer-html-mode)
 
-;; skewer mode
-(add-hook 'js2-mode-hook 'skewer-mode)
-(add-hook 'css-mode-hook 'skewer-css-mode)
-(add-hook 'html-mode-hook 'skewer-html-mode)
+;; (require 'simple-httpd)
+;; ;; set root folder for httpd server
+;; ;; TODO externalise this into a function
+;; (setq httpd-root "/Users/dboroujerdi/projects/Boids-js")
 
-(require 'simple-httpd)
-;; set root folder for httpd server
-;; TODO externalise this into a function
-(setq httpd-root "/Users/dboroujerdi/projects/Boids-js")
+;; ;; interactively modify html
+;; (add-hook 'html-mode-hook 'skewer-html-mode)
 
-;; interactively modify html
-(add-hook 'html-mode-hook 'skewer-html-mode)
+
+;;
+;; react development
+;;
+
+;; use web-mode for .jsx files
+
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; http://www.flycheck.org/manual/latest/index.html
+(require 'flycheck)
+
+;; turn on flychecking globally
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; customize flycheck temp file prefix
+(setq-default flycheck-temp-prefix ".flycheck")
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(json-jsonlist)))
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; for better jsx syntax-highlighting in web-mode
+;; - courtesy of Patrick @halbtuerke
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
 
 (provide 'init-js)
