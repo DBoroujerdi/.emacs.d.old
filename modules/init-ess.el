ba468992e7fcb9a8a989e0c15decf5cd6cb36dac
Daniel Boroujerdi <daniel.boroujerdi@gmail.com>
2017-02-27 13:01:43 +0000

    wrap ess in use-package

--- modules/init-ess.el
+++ modules/init-ess.el
@@ -1,8 +1,20 @@
+;;; package --- Summary
+;;; Commentary:
 ;; http://ess.r-project.org/Manual/ess.html#Step-by-step-instructions
 
 ;; M-x R RET -- Start R process or start another
 ;; C-c C-l   -- Load the current source file via source() into R process
 
-(require 'ess-site)
+
+;;; Code:
+
+(use-package ess-site
+  :mode "\\.r\\'"
+  :config
+  (progn
+    (require 'ess-site)
+    ))
+
 
 (provide 'init-ess)
+;;; init-ess.el ends here
